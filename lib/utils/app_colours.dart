2365c8ac14c58e52bfc88426605cd3642e2f14c9
TharudiHS <tharudihimeshaaa@gmail.com>
2025-09-10 13:59:33 +0530

    added new app colours dart file

---
+++ lib/utils/app_colours.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColours {
+  static const darkBlue = Color(0xFF131332);
+  static const navyBlue = Color(0xFF00004E);
+  static const white = Color(0xFFFFFFFF);
+  static const deepIndigo = Color(0xFF1B1B53);
+  static const pureRed = Color(0xFFFF0000);
+  static const transparentBlack = Color(0x29000000);
+  static const lightGrey = Color(0xFFC8C8C8);
+  static const crimsonRed = Color(0xFFD93232);
+}
