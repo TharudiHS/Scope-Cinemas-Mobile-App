7d7cbe65b0c6a1e98ba10fdfb64d8f1a49b8d53a
TharudiHS <tharudihimeshaaa@gmail.com>
2025-09-10 14:20:11 +0530

    updated the widget test dart file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:scope_cinemas/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ScopeCinemaApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
