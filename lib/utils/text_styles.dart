6394aa24f0ff56727096de0368aeac7167047308
TharudiHS <tharudihimeshaaa@gmail.com>
2025-09-10 13:58:54 +0530

    added new text styles dart file

---
+++ lib/utils/text_styles.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'app_colours.dart';
+
+class TextStyles {
+  static const TextStyle size24SofiaPro = TextStyle(
+    fontFamily: 'SofiaPro',
+    fontSize: 24,
+    fontWeight: FontWeight.w500, //medium
+    color: AppColours.white,
+    letterSpacing: 0,
+    height: 1.5,
+  );
+  static const TextStyle size16SofiaPro = TextStyle(
+    fontFamily: 'SofiaPro',
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: AppColours.white,
+    letterSpacing: 0,
+    height: 1.25,
+  );
+  static const TextStyle size17SofiaPro = TextStyle(
+    fontFamily: 'SofiaPro',
+    fontSize: 17,
+    fontWeight: FontWeight.w400, //regular
+    color: AppColours.white,
+    letterSpacing: 0,
+    height: 1.235,
+  );
+  static const TextStyle size14SofiaPro = TextStyle(
+    fontFamily: 'SofiaPro',
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: AppColours.crimsonRed,
+    letterSpacing: 0,
+  );
+  static const TextStyle size14SofiaProlightGrey = TextStyle(
+    fontFamily: 'SofiaPro',
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: AppColours.lightGrey,
+    letterSpacing: 0,
+  );
+  static const TextStyle size14SofiaProwhite = TextStyle(
+    fontFamily: 'SofiaPro',
+    fontSize: 14,
+    fontWeight: FontWeight.w300, //light
+    color: AppColours.white,
+    letterSpacing: 0,
+  );
+}
