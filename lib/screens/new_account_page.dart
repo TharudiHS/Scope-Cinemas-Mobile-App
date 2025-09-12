f19b03873ee8d2bea945c3f9ad74c894ca34e1c9
TharudiHS <tharudihimeshaaa@gmail.com>
2025-09-12 15:46:18 +0530

    added new page

---
+++ lib/screens/new_account_page.dart
@@ -0,0 +1,261 @@
+import 'package:flutter/material.dart';
+import 'package:scope_cinemas/components/main_button.dart';
+import 'package:scope_cinemas/screens/login_page.dart';
+import 'package:scope_cinemas/utils/text_styles.dart';
+import '../utils/app_colours.dart';
+
+class NewAccountPage extends StatefulWidget {
+  const NewAccountPage({super.key});
+
+  @override
+  State<NewAccountPage> createState() => _NewAccountPageState();
+}
+
+class _NewAccountPageState extends State<NewAccountPage> {
+  bool _obscurePassword = true;
+  bool _obscureConfirmPassword = true;
+  String? gender;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [AppColours.darkBlue, AppColours.navyBlue],
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Title
+                const SizedBox(height: 40),
+                const Text(
+                  "CREATE AN ACCOUNT",
+                  style: TextStyles.size24SofiaPro,
+                ),
+                const SizedBox(height: 12),
+                const Text(
+                  "Enter your details below to create a new account",
+                  style: TextStyles.size14SofiaProlightGrey,
+                ),
+                const SizedBox(height: 24),
+
+                // Form container
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(color: AppColours.deepIndigo),
+                  child: Column(
+                    children: [
+                      _buildTextField("First name", required: true),
+                      _buildTextField("Surname", required: true),
+                      _buildTextField("Email address", required: true),
+                      _buildTextField("Mobile number", required: true),
+                      _buildTextField(
+                        "Date of birth",
+                        suffixIcon: const Icon(
+                          Icons.calendar_today,
+                          color: AppColours.white,
+                          size: 20,
+                        ),
+                      ),
+
+                      const SizedBox(height: 12),
+                      // Gender radio
+                      Row(
+                        children: [
+                          const Text(
+                            "Gender",
+                            style: TextStyles.size14SofiaProwhite,
+                          ),
+                          const SizedBox(width: 20),
+                          Row(
+                            children: [
+                              Radio<String>(
+                                value: "Male",
+                                groupValue: gender,
+                                activeColor: Colors.amber,
+                                onChanged: (value) {
+                                  setState(() => gender = value);
+                                },
+                              ),
+                              const Text(
+                                "Male",
+                                style: TextStyles.size14SofiaProwhite,
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Radio<String>(
+                                value: "Female",
+                                groupValue: gender,
+                                activeColor: Colors.amber,
+                                onChanged: (value) {
+                                  setState(() => gender = value);
+                                },
+                              ),
+                              const Text(
+                                "Female",
+                                style: TextStyles.size14SofiaProwhite,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16),
+
+                // Password container
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(color: AppColours.deepIndigo),
+                  child: Column(
+                    children: [
+                      _buildPasswordField("Password", _obscurePassword, () {
+                        setState(() {
+                          _obscurePassword = !_obscurePassword;
+                        });
+                      }),
+                      _buildPasswordField(
+                        "Confirm password",
+                        _obscureConfirmPassword,
+                        () {
+                          setState(() {
+                            _obscureConfirmPassword = !_obscureConfirmPassword;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 24),
+
+                // account button
+                MainButton(label: "CREATE A NEW ACCOUNT", onPressed: () {}),
+
+                const SizedBox(height: 12),
+
+                // Already have account
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Already have an account? ",
+                      style: TextStyles.size14SofiaProwhite,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const LoginPage(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        "Login",
+                        style: TextStyles.size14SofiaPro.copyWith(
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 40),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField(
+    String label, {
+    bool required = false,
+    Widget? suffixIcon,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16),
+      child: TextField(
+        style: const TextStyle(color: AppColours.white),
+        decoration: InputDecoration(
+          label: RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: label,
+                  style: TextStyles.size17SofiaPro.copyWith(
+                    color: AppColours.white,
+                  ),
+                ),
+                if (required)
+                  const TextSpan(
+                    text: " *",
+                    style: TextStyle(color: Colors.red),
+                  ),
+              ],
+            ),
+          ),
+          suffixIcon: suffixIcon != null
+              ? Padding(padding: const EdgeInsets.all(12), child: suffixIcon)
+              : null,
+          enabledBorder: const UnderlineInputBorder(
+            borderSide: BorderSide(color: AppColours.lightGrey),
+          ),
+          focusedBorder: const UnderlineInputBorder(
+            borderSide: BorderSide(color: AppColours.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPasswordField(
+    String label,
+    bool obscure,
+    VoidCallback toggleVisibility,
+  ) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16),
+      child: TextField(
+        obscureText: obscure,
+        style: const TextStyle(color: AppColours.white),
+        decoration: InputDecoration(
+          label: RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: label,
+                  style: TextStyles.size17SofiaPro.copyWith(
+                    color: AppColours.white,
+                  ),
+                ),
+                const TextSpan(
+                  text: " *",
+                  style: TextStyle(color: Colors.red),
+                ),
+              ],
+            ),
+          ),
+
+          enabledBorder: const UnderlineInputBorder(
+            borderSide: BorderSide(color: AppColours.lightGrey),
+          ),
+          focusedBorder: const UnderlineInputBorder(
+            borderSide: BorderSide(color: AppColours.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
