1e552d909c70c3777ed13aa105f1a1355b686791
TharudiHS <tharudihimeshaaa@gmail.com>
2025-09-18 11:23:43 +0530

    finalized the bottom bar

--- lib/components/bottom_bar.dart
+++ lib/components/bottom_bar.dart
@@ -1,57 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:scope_cinemas/screens/movies_page.dart';
+import 'package:scope_cinemas/screens/home_page.dart';
 import 'package:scope_cinemas/utils/app_colours.dart';
 
-class CustomBottomBar extends StatelessWidget {
-  final int currentIndex;
-  final Function(int) onTap;
+class BottomNavBar extends StatelessWidget {
+  final int selectedIndex;
 
-  const CustomBottomBar({
-    super.key,
-    required this.currentIndex,
-    required this.onTap,
-  });
+  const BottomNavBar({super.key, required this.selectedIndex});
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      backgroundColor: AppColours.darkBlue,
-      selectedItemColor: AppColours.crimsonRed,
-      unselectedItemColor: Colors.white,
-      currentIndex: currentIndex,
-      onTap: onTap,
-      items: [
-        BottomNavigationBarItem(icon: const Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(
-          icon: Container(
-            decoration: BoxDecoration(
-              color: currentIndex == 1
-                  ? AppColours.crimsonRed
-                  : Colors.transparent,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            padding: const EdgeInsets.all(6),
-            child: const Icon(Icons.movie),
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      color: AppColours.darkNavy,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _svgNavItem("assets/images/home.svg", "Home", selectedIndex == 0, () {
+            if (selectedIndex != 1) {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (_) => const HomePage()),
+              );
+            }
+          }),
+          _svgNavItem(
+            "assets/images/movies.svg",
+            "Movies",
+            selectedIndex == 1,
+            () {
+              if (selectedIndex != 1) {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (_) => const MoviesPage()),
+                );
+              }
+            },
           ),
-          label: 'Movies',
-        ),
-        const BottomNavigationBarItem(
-          icon: Icon(Icons.location_on),
-          label: 'Cinemas',
-        ),
-        const BottomNavigationBarItem(
-          icon: Icon(Icons.local_drink),
-          label: 'Snacks',
-        ),
-        const BottomNavigationBarItem(icon: Icon(Icons.menu), label: 'Menu'),
-      ],
-      selectedLabelStyle: const TextStyle(
-        fontSize: 12,
-        fontWeight: FontWeight.w500,
+          _svgNavItem(
+            "assets/images/cinemas.svg",
+            "Cinemas",
+            selectedIndex == 2,
+            () {},
+          ),
+          _svgNavItem(
+            "assets/images/snacks.svg",
+            "Snacks",
+            selectedIndex == 3,
+            () {},
+          ),
+          _svgNavItem(
+            "assets/images/menu.svg",
+            "Menu",
+            selectedIndex == 4,
+            () {},
+          ),
+        ],
       ),
-      unselectedLabelStyle: const TextStyle(
-        fontSize: 12,
-        fontWeight: FontWeight.w400,
+    );
+  }
+
+  static Widget _svgNavItem(
+    String iconPath,
+    String label,
+    bool isSelected,
+    VoidCallback onTap,
+  ) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            width: 65,
+            height: 45,
+            decoration: BoxDecoration(
+              color: isSelected ? AppColours.crimsonRed : AppColours.darkBlue,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            padding: const EdgeInsets.all(12),
+            child: SvgPicture.asset(
+              iconPath,
+              height: 23,
+              width: 23,
+              color: isSelected ? AppColours.darkBlue : Colors.white,
+            ),
+          ),
+          const SizedBox(height: 6),
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: 10,
+              fontWeight: FontWeight.w500,
+              color: isSelected ? AppColours.crimsonRed : Colors.white,
+            ),
+          ),
+        ],
       ),
     );
   }
