2c9b7a7653bfdd4191a0fcdb811f6c86f94cdd4d
TharudiHS <tharudihimeshaaa@gmail.com>
2025-09-19 11:47:46 +0530

    finalized the code

--- lib/screens/new_account_page.dart
+++ lib/screens/new_account_page.dart
@@ -19,162 +19,165 @@ class _NewAccountPageState extends State<NewAccountPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [AppColours.darkBlue, AppColours.navyBlue],
+      backgroundColor: AppColours.darkBlue,
+      appBar: AppBar(
+        backgroundColor: AppColours.darkNavy,
+        leading: IconButton(
+          icon: Image.asset(
+            "assets/images/arrow_back.png",
+            width: 40,
+            height: 40,
+            color: Colors.white,
           ),
+          onPressed: () => Navigator.pop(context),
         ),
-        child: Center(
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                // Title
-                const SizedBox(height: 40),
-                const Text(
-                  "CREATE AN ACCOUNT",
-                  style: TextStyles.size24SofiaPro,
-                ),
-                const SizedBox(height: 12),
-                const Text(
-                  "Enter your details below to create a new account",
-                  style: TextStyles.size14SofiaProlightGrey,
-                ),
-                const SizedBox(height: 24),
+        centerTitle: true,
+        title: const Text(
+          "CREATE AN ACCOUNT",
+          style: TextStyles.size20SofiaProwhitemedium,
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 12),
+            const Text(
+              "Enter your details below to create a new account",
+              style: TextStyles.size14SofiaProwhite,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 24),
 
-                // Form container
-                Container(
-                  padding: const EdgeInsets.all(16),
-                  decoration: BoxDecoration(color: AppColours.deepIndigo),
-                  child: Column(
+            // Form container
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(color: AppColours.deepIndigo),
+              child: Column(
+                children: [
+                  _buildTextField("First name", required: true),
+                  _buildTextField("Surname", required: true),
+                  _buildTextField("Email address", required: true),
+                  _buildTextField("Mobile number", required: true),
+                  _buildTextField(
+                    "Date of birth",
+                    suffixIcon: const Icon(
+                      Icons.calendar_today,
+                      color: AppColours.white,
+                      size: 20,
+                    ),
+                  ),
+
+                  const SizedBox(height: 12),
+                  // Gender radio
+                  Row(
                     children: [
-                      _buildTextField("First name", required: true),
-                      _buildTextField("Surname", required: true),
-                      _buildTextField("Email address", required: true),
-                      _buildTextField("Mobile number", required: true),
-                      _buildTextField(
-                        "Date of birth",
-                        suffixIcon: const Icon(
-                          Icons.calendar_today,
-                          color: AppColours.white,
-                          size: 20,
-                        ),
+                      const Text(
+                        "Gender",
+                        style: TextStyles.size14SofiaProwhite,
                       ),
-
-                      const SizedBox(height: 12),
-                      // Gender radio
+                      const SizedBox(width: 20),
                       Row(
                         children: [
+                          Radio<String>(
+                            value: "Male",
+                            groupValue: gender,
+                            activeColor: AppColours.goldenYellow,
+                            onChanged: (value) {
+                              setState(() => gender = value);
+                            },
+                          ),
                           const Text(
-                            "Gender",
+                            "Male",
                             style: TextStyles.size14SofiaProwhite,
                           ),
-                          const SizedBox(width: 20),
-                          Row(
-                            children: [
-                              Radio<String>(
-                                value: "Male",
-                                groupValue: gender,
-                                activeColor: Colors.amber,
-                                onChanged: (value) {
-                                  setState(() => gender = value);
-                                },
-                              ),
-                              const Text(
-                                "Male",
-                                style: TextStyles.size14SofiaProwhite,
-                              ),
-                            ],
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Radio<String>(
+                            value: "Female",
+                            groupValue: gender,
+                            activeColor: AppColours.goldenYellow,
+                            onChanged: (value) {
+                              setState(() => gender = value);
+                            },
                           ),
-                          Row(
-                            children: [
-                              Radio<String>(
-                                value: "Female",
-                                groupValue: gender,
-                                activeColor: Colors.amber,
-                                onChanged: (value) {
-                                  setState(() => gender = value);
-                                },
-                              ),
-                              const Text(
-                                "Female",
-                                style: TextStyles.size14SofiaProwhite,
-                              ),
-                            ],
+                          const Text(
+                            "Female",
+                            style: TextStyles.size14SofiaProwhite,
                           ),
                         ],
                       ),
                     ],
                   ),
-                ),
-                const SizedBox(height: 16),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
 
-                // Password container
-                Container(
-                  padding: const EdgeInsets.all(16),
-                  decoration: BoxDecoration(color: AppColours.deepIndigo),
-                  child: Column(
-                    children: [
-                      _buildPasswordField("Password", _obscurePassword, () {
-                        setState(() {
-                          _obscurePassword = !_obscurePassword;
-                        });
-                      }),
-                      _buildPasswordField(
-                        "Confirm password",
-                        _obscureConfirmPassword,
-                        () {
-                          setState(() {
-                            _obscureConfirmPassword = !_obscureConfirmPassword;
-                          });
-                        },
+            // Password container
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(color: AppColours.deepIndigo),
+              child: Column(
+                children: [
+                  _buildPasswordField("Password", _obscurePassword, () {
+                    setState(() {
+                      _obscurePassword = !_obscurePassword;
+                    });
+                  }),
+                  _buildPasswordField(
+                    "Confirm password",
+                    _obscureConfirmPassword,
+                    () {
+                      setState(() {
+                        _obscureConfirmPassword = !_obscureConfirmPassword;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 80),
+
+            // Already have account
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Already have an account? ",
+                  style: TextStyles.size14SofiaProwhite,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const LoginPage(),
                       ),
-                    ],
+                    );
+                  },
+                  child: Text(
+                    "Login",
+                    style: TextStyles.size14SofiaPro.copyWith(
+                      color: AppColours.crimsonRed,
+                      decoration: TextDecoration.underline,
+                      decorationColor: AppColours.crimsonRed,
+                    ),
                   ),
                 ),
-
-                const SizedBox(height: 24),
-
-                // account button
-                MainButton(label: "CREATE A NEW ACCOUNT", onPressed: () {}),
-
-                const SizedBox(height: 12),
-
-                // Already have account
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "Already have an account? ",
-                      style: TextStyles.size14SofiaProwhite,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const LoginPage(),
-                          ),
-                        );
-                      },
-                      child: Text(
-                        "Login",
-                        style: TextStyles.size14SofiaPro.copyWith(
-                          color: Colors.red,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 40),
               ],
             ),
-          ),
+            const SizedBox(height: 12),
+          ],
         ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: MainButton(label: "CREATE A NEW ACCOUNT", onPressed: () {}),
       ),
     );
   }
@@ -201,7 +204,7 @@ class _NewAccountPageState extends State<NewAccountPage> {
                 if (required)
                   const TextSpan(
                     text: " *",
-                    style: TextStyle(color: Colors.red),
+                    style: TextStyle(color: AppColours.crimsonRed),
                   ),
               ],
             ),
@@ -242,12 +245,11 @@ class _NewAccountPageState extends State<NewAccountPage> {
                 ),
                 const TextSpan(
                   text: " *",
-                  style: TextStyle(color: Colors.red),
+                  style: TextStyle(color: AppColours.crimsonRed),
                 ),
               ],
             ),
           ),
-
           enabledBorder: const UnderlineInputBorder(
             borderSide: BorderSide(color: AppColours.lightGrey),
           ),
